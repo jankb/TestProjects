82369a07f5ea0b688ae76a7f9f49571259584f24
Jan Kristian Bueie <jan.kristian.bueie@gmail.com>
2018-01-23 21:23:49 +0000

    Some file descriptor-handling changes

--- RPi/pwmIC/main.cpp
+++ RPi/pwmIC/main.cpp
@@ -21,7 +21,8 @@ class GPIO
       m_exportPath("/sys/class/gpio/export"),
       m_unExportPath("/sys/class/gpio/unexport"),
       m_valuePath("/sys/class/gpio/"),
-      m_direction(dir)
+      m_direction(dir),
+      m_ValueFD(-1)
   {
     m_modeMap[IN] = "in"; 
     m_modeMap[OUT] = "out"; 
@@ -29,46 +30,72 @@ class GPIO
     m_directionPath = "/sys/class/gpio/gpio"+m_pin+"/direction";
     writeToPath(m_exportPath, m_pin);
     writeToPath(m_directionPath, m_modeMap[m_direction]);
+    m_ValueFD = openFD(m_valuePath);
   }
 
 
     ~GPIO()
     {
       writeToPath(m_unExportPath, m_pin);
+      closeFD(m_ValueFD);
     }
 
-   void turnOn()
+    void turnOn()
     {
-      writeToPath(m_valuePath, "1");
+      writeTo(m_ValueFD, "1");
     }
 
-   void turnOff()
+    void turnOff()
     {
-      writeToPath(m_valuePath, "0");
+      writeTo(m_ValueFD, "0");
     }
 
   private:
     std::map<MODE, std::string> m_modeMap;
 
-    void writeToPath(std::string path, std::string val)
+    int openFD(const std::string &path)
     {
-      std::cout << path <<" " << val << std::endl;
       int fd = open(path.c_str(), O_WRONLY);
-      if (fd == -1)
+      if (fd  == -1)
       {
         int err = errno;
         printf("Error %d opening %s.\n", err, path.c_str());
-        return;
-      } else
+      } 
+      return fd;
+    }
+
+    int closeFD(int fd)
+    {
+      int res = close(fd);
+      if (res  == -1)
       {
-        const char* pin = val.c_str();
-        ssize_t res = write(fd, pin, sizeof(pin));
-        if (res == -1)
-        {
-          int err = errno;
-          printf("Error %d writing %d to device %s.\n", err, pin, path.c_str());
-          return;
-        }
+        int err = errno;
+        printf("Error %d closing %d.\n", err, fd);
+      } 
+      return res;
+    }
+
+    int writeTo(int fd, const std::string &val)
+    {
+      const char* value = val.c_str();
+      ssize_t res = write(fd, value, sizeof(value));
+      if (res == -1)
+      {
+        int err = errno;
+        printf("Error %d writing %d to fd %d.\n", err, value, fd);
+      }
+      return res;
+    }
+
+
+    void writeToPath(std::string path, std::string val)
+    {
+      std::cout << path <<" " << val << std::endl;
+      int fd = openFD(path);
+      if (fd != -1)
+      {
+        writeTo(fd, val);
+        closeFD(fd);
       }
     }
 
@@ -77,6 +104,7 @@ class GPIO
     std::string m_valuePath;
     std::string m_directionPath;
     std::string m_pin;
+    int m_ValueFD;
     MODE m_direction;
 };
 
@@ -86,6 +114,8 @@ int main()
   std::cout << "Starting." << std::endl;
   GPIO gpio(13,GPIO::OUT);
   gpio.turnOn();
+  int value;
+  std::cin >> value;
   gpio.turnOff();
   return 0;
 }
